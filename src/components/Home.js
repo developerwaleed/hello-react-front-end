8cbd9d79e1b3b119c9bb87433e9d19c79a4aa7b5
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-23 00:45:06 +0500

    Add components

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1>Hello, Welcome!!</h1>
+      <button type="button" onClick={() => { navigate('/greeting'); }}>View a greeting</button>
+    </>
+  );
+};
+
+export default Home;
