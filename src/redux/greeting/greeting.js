79a24701a91bce1da562cd4b74272c056dfb0560
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-23 12:53:44 +0500

    Apply suggested changes

--- src/redux/greeting/greeting.js
+++ src/redux/greeting/greeting.js
@@ -1,6 +1,7 @@
 import GET_RANDOM_GREETING from '../actionTypes';
 
-const greeting = (action, state = {}) => {
+// eslint-disable-next-line default-param-last
+const greeting = (state = {}, action) => {
   switch (action.type) {
     case `${GET_RANDOM_GREETING}/fulfilled`:
       return action.payload;
